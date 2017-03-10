f24ba79c33efb9e963d647d4974ac012c7465a63
Maithilish <maithilish@gmail.com>
2017-03-10 20:55:41 +0530

    checkstyle changes - appender

--- src/main/java/in/m/picks/appender/Appender.java
+++ src/main/java/in/m/picks/appender/Appender.java
@@ -6,15 +6,26 @@ import in.m.picks.model.FieldsBase;
 
 public abstract class Appender implements Runnable {
 
-	protected List<FieldsBase> fields;
+    private List<FieldsBase> fields;
 
-	public enum Marker {
-		EOF
-	}
+    public enum Marker {
+        EOF
+    }
 
-	abstract public void append(Object object) throws InterruptedException;
+    public abstract void append(Object object) throws InterruptedException;
 
-	public void setFields(List<FieldsBase> fields) {
-		this.fields = fields;
-	}
+    /*
+     *
+     */
+    public void setFields(final List<FieldsBase> fields) {
+        this.fields = fields;
+    }
+
+    /*
+     *
+     */
+    protected List<FieldsBase> getFields() {
+        return fields;
+    }
+
 }
--- src/main/java/in/m/picks/appender/FileAppender.java
+++ src/main/java/in/m/picks/appender/FileAppender.java
@@ -17,44 +17,47 @@ import in.m.picks.shared.MonitorService;
 import in.m.picks.util.FieldsUtil;
 import in.m.picks.util.Util;
 
-public class FileAppender extends Appender {
+public final class FileAppender extends Appender {
 
-	final static Logger logger = LoggerFactory.getLogger(FileAppender.class);
+    static final Logger LOGGER = LoggerFactory.getLogger(FileAppender.class);
 
-	private BlockingQueue<Object> queue;
+    private static final int QUEUE_CAPACITY = 1024;
 
-	public FileAppender() {
-		queue = new ArrayBlockingQueue<Object>(1024);
-	}
+    private BlockingQueue<Object> queue;
 
-	@Override
-	public void run() {
-		PrintWriter writer = null;
-		try {
-			String file = FieldsUtil.getValue(fields, "file");
-			FileOutputStream fos = FileUtils.openOutputStream(new File(file));
-			writer = new PrintWriter(fos);
-			for (;;) {
-				Object item = queue.take();
-				if (item == Marker.EOF) {
-					break;
-				}
-				String str = item.toString();
-				writer.println(str);
-			}
-		} catch (FieldNotFoundException | IOException
-				| InterruptedException e) {
-			String message = "file appender ";
-			logger.error("{} {}", message, Util.getMessage(e));
-			logger.debug("{}", e);
-			MonitorService.INSTANCE.addActivity(Type.GIVENUP, message, e);
-		} finally {
-			writer.close();
-		}
-	}
+    public FileAppender() {
 
-	@Override
-	public void append(Object object) throws InterruptedException {
-		queue.put(object);
-	}
+        queue = new ArrayBlockingQueue<Object>(QUEUE_CAPACITY);
+    }
+
+    @Override
+    public void run() {
+        PrintWriter writer = null;
+        try {
+            String file = FieldsUtil.getValue(getFields(), "file");
+            FileOutputStream fos = FileUtils.openOutputStream(new File(file));
+            writer = new PrintWriter(fos);
+            for (;;) {
+                Object item = queue.take();
+                if (item == Marker.EOF) {
+                    break;
+                }
+                String str = item.toString();
+                writer.println(str);
+            }
+        } catch (FieldNotFoundException | IOException
+                | InterruptedException e) {
+            String message = "file appender ";
+            LOGGER.error("{} {}", message, Util.getMessage(e));
+            LOGGER.debug("{}", e);
+            MonitorService.INSTANCE.addActivity(Type.GIVENUP, message, e);
+        } finally {
+            writer.close();
+        }
+    }
+
+    @Override
+    public void append(final Object object) throws InterruptedException {
+        queue.put(object);
+    }
 }
